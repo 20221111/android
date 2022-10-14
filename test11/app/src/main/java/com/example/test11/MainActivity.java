7b1e54530c069b47eb7bb094fd977f8909414120
WooyoungKwon <rnjsdndud00@inha.edu>
2022-10-14 19:25:45 +0900

    ver1

---
+++ test11/app/src/main/java/com/example/test11/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.test11;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button NewAccount = findViewById(R.id.NewAccount);
+        NewAccount.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), Create_Account.class);
+                startActivity(intent);
+            }
+        });
+        }
+}
