9ea22e7c9da4f64537092b9b52fa6a33368be2cc
WooyoungKwon <rnjsdndud00@inha.edu>
2022-10-14 23:26:08 +0900

    20221014

--- test11/app/src/main/java/com/example/test11/MainActivity.java
+++ test11/app/src/main/java/com/example/test11/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
 
         //New account 클릭시 sign up 페이지로 이동
         Button NewAccount = findViewById(R.id.NewAccount);
