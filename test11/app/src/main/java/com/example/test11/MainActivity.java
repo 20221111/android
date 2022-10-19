47358fa16f361a5c30424d676a0d6a78642536c6
WooyoungKwon <rnjsdndud00@inha.edu>
2022-10-19 17:55:33 +0900

    20221019
    로그인 성공시 캘린더 화면 출력

--- test11/app/src/main/java/com/example/test11/MainActivity.java
+++ test11/app/src/main/java/com/example/test11/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
         //New account 클릭시 sign up 페이지로 이동
         Button NewAccount = findViewById(R.id.NewAccount);
         NewAccount.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(getApplicationContext(), Create_Account.class);
@@ -26,14 +26,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //ForgotPassword 클릭시 forgotpassword 페이지로 이동
         Button ForgotPassword = findViewById(R.id.ForgotPassword);
         ForgotPassword.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view){
                 Intent intent = new Intent(getApplicationContext(), Forgot_Password.class);
                 startActivity(intent);
             }
         });
+
+        //Sign in 클릭시 activity_main 페이지로 이동
+        ImageButton Sign_in = (ImageButton) findViewById(R.id.Sign_in);
+        Sign_in.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(getApplicationContext(), Calender_btn.class);
+                startActivity(intent);
+            }
+        });
+
+
         }
     }
